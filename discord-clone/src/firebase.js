ed3e3f4ad453f16cc938084411ea51ca73b95c4b
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-11-04 17:37:21 +0530

    commit

--- discord-clone/src/firebase.js
+++ discord-clone/src/firebase.js
@@ -1,19 +1,19 @@
-import firebase from "./firebase"
+import firebase from "./firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCSelqX5aLeyaAIabuStJ0bcyCQiNqIxPI",
-    authDomain: "discord-clone-9d6c2.firebaseapp.com",
-    projectId: "discord-clone-9d6c2",
-    storageBucket: "discord-clone-9d6c2.appspot.com",
-    messagingSenderId: "31709035198",
-    appId: "1:31709035198:web:4635c1d74c2d41df253cef",
-    measurementId: "G-SVB7VMJ9X7"
-  };
+  apiKey: "AIzaSyCSelqX5aLeyaAIabuStJ0bcyCQiNqIxPI",
+  authDomain: "discord-clone-9d6c2.firebaseapp.com",
+  projectId: "discord-clone-9d6c2",
+  storageBucket: "discord-clone-9d6c2.appspot.com",
+  messagingSenderId: "31709035198",
+  appId: "1:31709035198:web:4635c1d74c2d41df253cef",
+  measurementId: "G-SVB7VMJ9X7"
+};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db =  firebaseApp.firestore();
   const auth = firebase.auth();
   const provider = new firebase.auth.GoogleAuthProvider();
 
-  export { auth, provider }
-  export default db 
+  export { auth, provider };
+  export default db;
